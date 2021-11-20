82710724eff312f75eab57bc5c9b1a0ab8c01b50
dannyx-hub <daro322.dp@gmail.com>
2021-11-20 16:58:27 +0100

    add more options to config

--- config.py
+++ config.py
@@ -1,5 +1,21 @@
 import json
+import os, sys
 def JSONimport():
-    with open("config.json","r") as jsonconfig:
-        config=json.load(jsonconfig)
-    return config["path"]
+    if os.path.isfile("config.json"): 
+        with open("config.json","r") as jsonconfig:
+            config=json.load(jsonconfig)
+            path = config["path"]
+            if len(path) == 0:
+                print("[*] path is empty. Please change your config file and rerun this shit")
+                sys.exit(1)
+            else:
+                    return path
+    else:
+        print("[*] ERROR: config file isn't exist. We change that and create this shit for you gl")
+        with open("config.json", "w") as jsonconfig:
+            data = '{"path":"D:\\skrypty\\platform-tools\\adb.exe start-server" }'
+            
+            json.dump(data,jsonconfig)
+    
+    
+JSONimport()
